590d4dca61d372ed7ce531541f8818d0a79d69ad
YASHRAUTELA <yashrautela1@gmail.com>
2022-04-11 19:32:13 +0530

    mysql connectivity

--- src/controller/AppController.ts
+++ src/controller/AppController.ts
@@ -1,8 +1,19 @@
-import { Request, Response } from "express";
-
+import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../config/error";
+import { query } from "../config/mysql";
+import { formattedResponse } from "../utils/utils";
 class AppController {
-    getData(request: Request, response: Response) {
-        response.json({ data: "App Success!" });
+    async getData(request: Request, response: Response, next: NextFunction) {
+        try {
+            const data = await query("SELECST * from users where email=?", ["abc@gmail.com"]);
+            response.json(formattedResponse(200, data, "User data fetched successfully"));
+        } catch (error: CustomError | any) {
+            if (error instanceof Error) {
+                throw new CustomError(error.message, 500, error.stack);
+            }
+            response.statusCode = error.code || 500;
+            response.json(error);
+        }
     }
 }
 
